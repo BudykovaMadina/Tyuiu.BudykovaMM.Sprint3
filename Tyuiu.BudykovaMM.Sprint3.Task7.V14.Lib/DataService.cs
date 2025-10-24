7b480054810b3ec7d5bb9965c5e796928242dd34
Мадина <mbudykova@mail.ru>
2025-10-25 00:09:34 +0500

    Task7

---
+++ Tyuiu.BudykovaMM.Sprint3.Task7.V14.Lib/DataService.cs
@@ -0,0 +1,30 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+namespace Tyuiu.BudykovaMM.Sprint3.Task7.V14.Lib
+{
+    public class DataService : ISprint3Task7V14
+    {
+        public double[] GetMassFunction(int startValue, int stopValue)
+        {
+            double[] valueArray;
+            int len = (stopValue - startValue) + 1;
+            valueArray = new double[len];
+            double y;
+            int count = 0;
+            for ( int x = startValue; x <= stopValue; x++)
+            {
+                if ((Math.Sin(x) + 1) == 0)
+                {
+                    valueArray[count] = 0;
+                }
+                else
+                {
+                    y = Math.Round(2 * x - 4 + (2 * x - 1) / (Math.Sin(x) + 1), 2);
+                    valueArray[count] = y;
+                }
+                count++;
+
+            }
+            return valueArray;
+        }
+    }
+}
---
+++ Tyuiu.BudykovaMM.Sprint3.Task7.V14/Program.cs
@@ -0,0 +1,50 @@
+﻿using Tyuiu.BudykovaMM.Sprint3.Task7.V14.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #3 │ Выполнила: Будыкова М. М. │ ПИНб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #3                                                               *");
+        Console.WriteLine("* Тема: Добавление к решению итоговых проектов по спринту                 *");
+        Console.WriteLine("* Вариант #14                                                             *");
+        Console.WriteLine("* Выполнила: Будыкова Мадина Маратовна │ ПИНб-25-1                        *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу, которая таблицу значений функции                    *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        int startValue = -5;
+        int stopValue = 5;
+
+        Console.WriteLine("Старт шага = " + startValue);
+        Console.WriteLine("Конец шага = " + stopValue);
+
+        int len = ds.GetMassFunction(startValue, stopValue).Length;
+
+        double[] valueArray;
+        valueArray = new double[len];
+
+        valueArray = ds.GetMassFunction(startValue, stopValue);
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("+----------+----------+");
+        Console.WriteLine("|    X     |   f(x)   |");
+        Console.WriteLine("+----------+----------+");
+        for (int i = 0; i<=len-1;i++)
+        {
+            Console.WriteLine("|{0,5:d}     |  {1, 6:f2}  |", startValue, valueArray[i]);
+            startValue++;
+        }
+        Console.WriteLine("+----------+----------+");
+        Console.ReadKey();
+    }
+}
