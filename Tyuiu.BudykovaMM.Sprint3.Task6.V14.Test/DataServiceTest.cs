84270f3400ed00d0c78e86f1625fd83e52be8601
Мадина <mbudykova@mail.ru>
2025-10-24 23:07:08 +0500

    Task6

---
+++ Tyuiu.BudykovaMM.Sprint3.Task6.V14.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using Tyuiu.BudykovaMM.Sprint3.Task6.V14.Lib;
+namespace Tyuiu.BudykovaMM.Sprint3.Task6.V14.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetSumTheDivisors()
+        {
+            DataService ds = new DataService();
+
+            int startValue = 7;
+            int stopValue = 16;
+
+            int res = ds.GetSumTheDivisors(startValue, stopValue);
+
+            int wait = 13;
+
+            Assert.AreEqual(wait, res);
+
+        }
+    }
+}
---
+++ Tyuiu.BudykovaMM.Sprint3.Task6.V14/Program.cs
@@ -0,0 +1,36 @@
+﻿using Tyuiu.BudykovaMM.Sprint3.Task6.V14.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #3 │ Выполнила: Будыкова М. М. │ ПИНб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #3                                                               *");
+        Console.WriteLine("* Тема: Обработка целочисленной информации                                *");
+        Console.WriteLine("* Вариант #14                                                             *");
+        Console.WriteLine("* Выполнила: Будыкова Мадина Маратовна │ ПИНб-25-1                        *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Напишите программу, которая ищет среди целых чисел,принадлежащих        *");
+        Console.WriteLine("* числовому отрезку [7, 16] количество всех делителей больше 5            *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        
+        int startValue = 7;
+        int stopValue = 16;
+
+        Console.WriteLine("Начало отрезка = " + startValue);
+        Console.WriteLine("Конец отрезка = " + stopValue);
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("Искомое количество делителей = " + ds.GetSumTheDivisors(startValue, stopValue));
+        Console.ReadKey();
+    }
+}
