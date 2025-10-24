ed58394e575652bb3db5d109b7064087ab5b747c
Мадина <mbudykova@mail.ru>
2025-10-24 20:58:21 +0500

    Task3

---
+++ Tyuiu.BudykovaMM.Sprint3.Task3.V24.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using Tyuiu.BudykovaMM.Sprint3.Task3.V24.Lib;
+namespace Tyuiu.BudykovaMM.Sprint3.Task3.V24.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidReplaceCharInString()
+        {
+            DataService ds = new DataService();
+
+            string value = "gft hggt ntg";
+            char replaceable = 'g';
+            char replacement = '*';
+
+            string result = ds.ReplaceCharInString(value, replaceable, replacement);
+            string wait = "*ft h**t nt*";
+
+            Assert.AreEqual(wait, result);
+
+        }
+    }
+}
---
+++ Tyuiu.BudykovaMM.Sprint3.Task3.V24/Program.cs
@@ -0,0 +1,37 @@
+﻿using Tyuiu.BudykovaMM.Sprint3.Task3.V24.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #3 │ Выполнила: Будыкова М. М. │ ПИНб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #3                                                               *");
+        Console.WriteLine("* Тема: Оператор цикла foreach                                            *");
+        Console.WriteLine("* Вариант #24                                                             *");
+        Console.WriteLine("* Выполнила: Будыкова Мадина Маратовна │ ПИНб-25-1                        *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Используя цикл foreach заменить буквы g  на * в строке: gft hggt ntg    *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        string value = "gft hggt ntg";
+        char replaceable = 'g';
+        char replacement = '*';
+
+        Console.WriteLine("Исходная строка = " + value);
+        Console.WriteLine("Старый символ = " + replaceable);
+        Console.WriteLine("Новый символ = " + replacement);
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("Обновлённая строка = " + ds.ReplaceCharInString(value, replaceable, replacement));
+        Console.ReadKey();
+    }
+}
